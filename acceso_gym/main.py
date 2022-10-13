0a88f8e56b19195675b34e0191eab1f4442d6328
AngelaArdila <anelacristi@gmail.com>
2022-10-13 12:04:38 -0500

    agregar plantilla flask

---
+++ acceso_gym/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if "main" in __name__:
+    app.run()
